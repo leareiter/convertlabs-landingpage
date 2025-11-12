305972a12c731acfa128dc5d8ae047f5a497a358
léa <lea.reiter@outlook.fr>
2025-11-12 17:34:49 +0100

    update: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -26,6 +26,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.9,
     },
+    {
+      url: 'https://convertlabs.fr/legal',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
   ]
 }
 
